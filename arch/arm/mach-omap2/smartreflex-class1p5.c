5348be72b2a651ba2de1c9ea6560bfd2ad99ae12
franciscofranco <franciscofranco.1990@gmail.com>
2012-06-07 04:33:17 +0100

    smartreflex-class1p5.c: change the check from only > 1350mV to >= 1350mV

--- arch/arm/mach-omap2/smartreflex-class1p5.c
+++ arch/arm/mach-omap2/smartreflex-class1p5.c
@@ -26,6 +26,8 @@
 #include <linux/workqueue.h>
 #include <linux/slab.h>
 #include <linux/opp.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
 
 #include "smartreflex.h"
 #include "voltage.h"
@@ -418,7 +420,7 @@ static int sr_class1p5_enable(struct voltagedomain *voltdm,
 		return 0;
 
 	/* Based on Imoseyon's idea to properly calibrate high frequencies e.g. >= 1.5Ghz MPU */
-	if (volt_data->volt_nominal > 1350000) {
+	if (volt_data->volt_nominal >= 1350000) {
 		volt_data->volt_calibrated = volt_data->volt_nominal;
 		volt_data->volt_dynamic_nominal = volt_data->volt_nominal;
 		pr_info("[franciscofranco] %p - nominal %d", __func__, volt_data->volt_nominal);
